8eba30d8aa4c84dc32b4d91cd34f1d683d70bfae
Hibaboudiaj <boudiajhiba2@gmail.com>
2026-03-10 13:09:56 +0100

    create server

---
+++ server.js
@@ -0,0 +1,22 @@
+//start express  .  connect mongodb  .  load routes//
+
+const express = require("express");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/auth.routes");
+const subscriptionRoutes = require("./routes/subscription.routes");
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/auth", authRoutes);
+app.use("/subscriptions", subscriptionRoutes);
+
+app.listen(8000, () => {
+  console.log("Server running on port http://localhost:8000 🚀");
+});
